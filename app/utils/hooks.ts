a6d41d1df73b4ede04cffbe0043e6dbabbbf67c1
ccmalcom <chasecmalcom@gmail.com>
2024-03-04 22:52:35 -0500

    handling no location on call

--- app/utils/hooks.ts
+++ app/utils/hooks.ts
@@ -1,12 +1,18 @@
 'use client'
 import useSWR from 'swr';
 import { Artist } from './types';
+import { getLatLong } from './actions';
 
 const fetcher = (url: string) => fetch(url).then(res => res.json());
-const fetcherWithArtists = async ([url, artistNames, userSettings]: [string, string[], object]) => {
+const fetcherWithArtists = async ([url, artistNames, userSettings]: [string, string[], { location: string, city: string, distance: string }]) => {
     // Your fetching logic here, using `url` and `artistNames`
-    let stringifiedArtists = JSON.stringify(artistNames);
-    let stringifiedUserSettings = JSON.stringify(userSettings);
+    if (!userSettings.location) {
+        let location = await getLatLong(userSettings.city);
+        userSettings.location = location.latitude + ',' + location.longitude;
+    }
+    if (!userSettings.distance) {
+        userSettings.distance = '50';
+    }
     const response = await fetch(url, {
         method: 'POST',
         headers: {
@@ -21,10 +27,10 @@ const fetcherWithArtists = async ([url, artistNames, userSettings]: [string, str
 
 export function useUser() {
     console.log('###HOOK### useUser function hit');
-    const { data, error, isLoading } = useSWR('/spotify/user', fetcher,{
-        dedupingInterval: 60000, 
-        revalidateOnFocus: false, 
-        revalidateOnReconnect: false, 
+    const { data, error, isLoading } = useSWR('/spotify/user', fetcher, {
+        dedupingInterval: 60000,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
     });
     return {
         user: data,
@@ -35,10 +41,10 @@ export function useUser() {
 
 export function useTopArtists() {
     console.log('###HOOK### useTopArtists function hit');
-    const { data, error, isLoading } = useSWR('/spotify/top-artists', fetcher,{
-        dedupingInterval: 60000, 
-        revalidateOnFocus: false, 
-        revalidateOnReconnect: false, 
+    const { data, error, isLoading } = useSWR('/spotify/top-artists', fetcher, {
+        dedupingInterval: 60000,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
     });
     return {
         topArtists: data,
@@ -49,10 +55,10 @@ export function useTopArtists() {
 
 export function useTopTracks() {
     console.log('###HOOK### useTopTracks function hit');
-    const { data, error, isLoading } = useSWR('/spotify/top-tracks', fetcher,{
-        dedupingInterval: 60000, 
-        revalidateOnFocus: false, 
-        revalidateOnReconnect: false, 
+    const { data, error, isLoading } = useSWR('/spotify/top-tracks', fetcher, {
+        dedupingInterval: 60000,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
     });
     return {
         topTracks: data,
@@ -65,10 +71,10 @@ export function useEvents(artistNames: string[], userSettings: { location: strin
     console.log('###HOOK### useEvents function hit');
     // const { data: orders } = useSWR(user ? ['/api/orders', user] : null, fetchWithUser)
     // const artistNames = topArtists.map(artist => artist.name);
-    const { data, error, isLoading } = useSWR(['/ticketmaster/events', artistNames, userSettings], fetcherWithArtists,{
-        dedupingInterval: 60000, 
-        revalidateOnFocus: false, 
-        revalidateOnReconnect: false, 
+    const { data, error, isLoading } = useSWR(['/ticketmaster/events', artistNames, userSettings], fetcherWithArtists, {
+        dedupingInterval: 60000,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
     });
     return {
         events: data,
