bc227b9eed2ed3000a92f4ac4d5cc925d2dd87a3
ccmalcom <chasecmalcom@gmail.com>
2024-05-23 15:19:31 -0400

    should finally work

--- app/ui/Dashboard/TimeRangeSelector.tsx
+++ app/ui/Dashboard/TimeRangeSelector.tsx
@@ -1,35 +1,46 @@
-// 'use client'
-import React from 'react';
+'use client'
+import React, { useState, useEffect } from 'react';
 import DropdownInput from '@/app/ui/DropdownInput';
 
 const TimeRangeSelector: React.FC = () => {
-    // const [selectedTimeRange, setSelectedTimeRange] = useState('');
-    if (typeof window !== undefined) {
-        const timeRange = localStorage.getItem('timeRange');
-        let selectedTimeRange = timeRange ? timeRange : 'short_term';
-        const timeRanges = [
-            { value: 'short_term', label: 'Month' },
-            { value: 'medium_term', label: '6 Months' },
-            { value: 'long_term', label: 'Year' },
-        ];
-        
-        const handleChange = (value: string) => {
-            selectedTimeRange = value;
-            localStorage.setItem('timeRange', value);
-            //refresh the page
-            window.location.reload();
-        };
-        
-        return (
-            <DropdownInput
+    const [selectedTimeRange, setSelectedTimeRange] = useState('short_term');
+    const getLocalStorage = () => {
+        if (typeof window !== undefined) {
+            if (localStorage.getItem('timeRange') !== null) {
+                return localStorage.getItem('timeRange') as string;
+            }
+        }
+        return 'short_term';
+    };
+
+    useEffect(() => {
+        let term = getLocalStorage();
+        setSelectedTimeRange(term);
+    }, []);
+    
+
+    const timeRanges = [
+        { value: 'short_term', label: 'Month' },
+        { value: 'medium_term', label: '6 Months' },
+        { value: 'long_term', label: 'Year' },
+    ];
+
+    const handleChange = (value: string) => {
+        setSelectedTimeRange(value);
+        localStorage.setItem('timeRange', value);
+        //refresh the page
+        window.location.reload();
+    };
+
+    return (
+        <DropdownInput
             id="time-range-selector"
             label="Time Range:"
             options={timeRanges}
             selectedValue={selectedTimeRange}
             onChange={handleChange}
-            />
-        );
-    };
+        />
+    );
 };
 
 export default TimeRangeSelector;
